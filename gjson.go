5cd723d566400bc7ac450eb0c5b9edbb75a70750
Josh Baker <joshbaker77@gmail.com>
2017-12-22 07:19:48 -0700

    simplify getmanybytes

--- gjson.go
+++ gjson.go
@@ -1610,11 +1610,7 @@ func GetMany(json string, path ...string) []Result {
 // The return value is a Result array where the number of items
 // will be equal to the number of input paths.
 func GetManyBytes(json []byte, path ...string) []Result {
-	res := make([]Result, len(path))
-	for i, path := range path {
-		res[i] = GetBytes(json, path)
-	}
-	return res
+	return GetMany(string(json), path...)
 }
 
 var fieldsmu sync.RWMutex
