512e0c1ecb731107327916c2415350281122b923
Shenal Fernando <shenal.fernando10@gmail.com>
2024-09-04 11:37:20 +0530

    new functiinality added to get many fields into map

--- gjson.go
+++ gjson.go
@@ -4,6 +4,7 @@ package gjson
 import (
 	"strconv"
 	"strings"
+	"sync"
 	"time"
 	"unicode/utf16"
 	"unicode/utf8"
@@ -957,7 +958,7 @@ func isDotPiperChar(s string) bool {
 				break
 			}
 		}
-		_, ok := modifiers[s[1:i]]
+		_, ok := modifiers.Load(s[1:i])
 		return ok
 	}
 	return c == '[' || c == '{'
@@ -1917,16 +1918,6 @@ func appendHex16(dst []byte, x uint16) []byte {
 	)
 }
 
-// DisableEscapeHTML will disable the automatic escaping of certain
-// "problamatic" HTML characters when encoding to JSON.
-// These character include '>', '<' and '&', which get escaped to \u003e,
-// \u0026, and \u003c respectively.
-//
-// This is a global flag and will affect all further gjson operations.
-// Ideally, if used, it should be set one time before other gjson functions
-// are called.
-var DisableEscapeHTML = false
-
 // AppendJSONString is a convenience function that converts the provided string
 // to a valid JSON string and appends it to dst.
 func AppendJSONString(dst []byte, s string) []byte {
@@ -1950,8 +1941,7 @@ func AppendJSONString(dst []byte, s string) []byte {
 				dst = append(dst, 'u')
 				dst = appendHex16(dst, uint16(s[i]))
 			}
-		} else if !DisableEscapeHTML &&
-			(s[i] == '>' || s[i] == '<' || s[i] == '&') {
+		} else if s[i] == '>' || s[i] == '<' || s[i] == '&' {
 			dst = append(dst, '\\', 'u')
 			dst = appendHex16(dst, uint16(s[i]))
 		} else if s[i] == '\\' {
@@ -2350,6 +2340,14 @@ func GetMany(json string, path ...string) []Result {
 	res := make([]Result, len(path))
 	for i, path := range path {
 		res[i] = Get(json, path)
+	}
+	return res
+}
+
+func GetManyIntoMap(json string, paths ...string) map[string]Result {
+	res := make(map[string]Result, len(paths))
+	for _, path := range paths {
+		res[path] = Get(json, path)
 	}
 	return res
 }
@@ -2763,7 +2761,8 @@ func execModifier(json, path string) (pathOut, res string, ok bool) {
 			break
 		}
 	}
-	if fn, ok := modifiers[name]; ok {
+	if fn, ok := modifiers.Load(name); ok {
+		function := fn.(func(json, arg string) string)
 		var args string
 		if hasArgs {
 			var parsedArgs bool
@@ -2794,7 +2793,7 @@ func execModifier(json, path string) (pathOut, res string, ok bool) {
 				pathOut = pathOut[i:]
 			}
 		}
-		return pathOut, fn(json, args), true
+		return pathOut, function(json, args), true
 	}
 	return pathOut, res, false
 }
@@ -2811,50 +2810,46 @@ func unwrap(json string) string {
 // DisableModifiers will disable the modifier syntax
 var DisableModifiers = false
 
-var modifiers map[string]func(json, arg string) string
+var modifiers sync.Map
 
 func init() {
-	modifiers = map[string]func(json, arg string) string{
-		"pretty":  modPretty,
-		"ugly":    modUgly,
-		"reverse": modReverse,
-		"this":    modThis,
-		"flatten": modFlatten,
-		"join":    modJoin,
-		"valid":   modValid,
-		"keys":    modKeys,
-		"values":  modValues,
-		"tostr":   modToStr,
-		"fromstr": modFromStr,
-		"group":   modGroup,
-		"dig":     modDig,
-	}
+	modifiers.Store("pretty", modPretty)
+	modifiers.Store("ugly", modUgly)
+	modifiers.Store("reverse", modReverse)
+	modifiers.Store("this", modThis)
+	modifiers.Store("flatten", modFlatten)
+	modifiers.Store("join", modJoin)
+	modifiers.Store("valid", modValid)
+	modifiers.Store("keys", modKeys)
+	modifiers.Store("values", modValues)
+	modifiers.Store("tostr", modToStr)
+	modifiers.Store("fromstr", modFromStr)
+	modifiers.Store("group", modGroup)
+	modifiers.Store("dig", modDig)
 }
 
 // AddModifier binds a custom modifier command to the GJSON syntax.
-// This operation is not thread safe and should be executed prior to
-// using all other gjson function.
 func AddModifier(name string, fn func(json, arg string) string) {
-	modifiers[name] = fn
+	modifiers.Store(name, fn)
 }
 
 // ModifierExists returns true when the specified modifier exists.
 func ModifierExists(name string, fn func(json, arg string) string) bool {
-	_, ok := modifiers[name]
+	_, ok := modifiers.Load(name)
 	return ok
 }
 
 // DeleteModifier delete a custom modifier command
 // Modifier can be deleted when ever needed
 func DeleteModifier(name string) {
-	delete(modifiers, name)
+	modifiers.Delete(name)
 }
 
 // DeleteModifierIfExists delete a custom modifier command if it is existing
 func DeleteModifierIfExists(name string) {
-	_, ok := modifiers[name]
+	_, ok := modifiers.Load(name)
 	if ok {
-		delete(modifiers, name)
+		modifiers.Delete(name)
 	}
 }
 
