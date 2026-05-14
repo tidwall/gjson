896d5690900cfeec65439415abc97a94f018b258
tidwall <tidwall@noreply.codeberg.org>
2026-05-14 17:00:10 +0200

    Update gjson.go

--- gjson.go
+++ gjson.go
@@ -2,7 +2,7 @@
 // Use of this source code is governed by an MIT-style
 // license that can be found in the LICENSE file.
 //
-// https://github.com/tidwall/gjson
+// https://codeberg.com/tidwall/gjson
 
 // Package gjson provides searching for json strings.
 package gjson
